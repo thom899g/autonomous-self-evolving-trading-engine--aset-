92c03e4829f2e5014016010f660d1078e7dae7d5
thom899g <153515950+thom899g@users.noreply.github.com>
2026-02-26 13:09:38 +0100

    Automated commit by HiveMind

---
+++ aset__init__.py
@@ -0,0 +1,6 @@
+"""
+Autonomous Self-Evolving Trading Engine (ASET)
+A market symbiosis engine that autonomously evolves trading strategies
+through reinforcement learning and genetic algorithms.
+"""
+__version__ = "1.0.0"
